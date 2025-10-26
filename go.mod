6662feb20057edf86586451146ada7c71e5e9b9d
magwach <emmanuelmagothe@gmail.com>
2025-10-26 16:40:44 +0300

    feat : Added the server.

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/magwach/rss-aggregator
+
+go 1.25.3
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.2.3
+
+require github.com/go-chi/cors v1.2.2 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	godotenv "github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello")
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in the enviroment")
+	}
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running! 🚀"))
+	})
+
+	fmt.Println("Listening on port:", portString)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
